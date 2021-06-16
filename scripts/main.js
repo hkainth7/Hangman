13abcecf6e75138be3060ce3968aa11a47111d0a
Harjit Kainth <harjitkainth10@gmail.com>
2021-06-16 16:55:26 -0700

    created hangman images, updated styles.scss and main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 const letters = document.getElementById("letters-container");
+const incorrectGuessCounter = document.getElementById("wrong-guesses");
+const guessContainer = document.getElementById("incorrect-guesses");
 
 const alphabet = [
     "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
@@ -13,3 +15,13 @@ alphabet.forEach(function(letter){
     button.appendChild(buttonValue);
     letters.appendChild(button);
 })
+
+
+if(incorrectGuessCounter.textContent < 2){
+    guessContainer.style.backgroundColor = "#E1E5EA";
+} else if(incorrectGuessCounter.textContent > 2 && incorrectGuessCounter.textContent < 5) {
+    guessContainer.style.backgroundColor = "#F5A962";
+}else{
+    guessContainer.style.backgroundColor = "red";
+}
+
