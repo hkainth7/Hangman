dae67afebe6796ba3c13ce3c7cf6e5f2f17c89e6
Harjit Kainth <harjitkainth10@gmail.com>
2021-06-19 12:18:16 -0700

    updated index.html, update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,22 @@ guessesColorChange();
 
 const wrongGuesses = document.getElementById("wrong-guesses");
 wrongGuesses.innerText = 0;
+
+const fetchFileURL = 'data/json/wordsapi_sample.json';
+
+fetch(fetchFileURL)
+    .then(function(response){
+        console.log('response ok');
+        if(response.ok){
+            let responseAsJson = response.json();
+            return responseAsJson;
+        }else{
+            console.log('error with response');
+        }
+    })
+    .then(function( jsonData ){
+        
+    })
+    .catch(function(){
+        console.log('error with response');
+    }); 
