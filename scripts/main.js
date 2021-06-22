01cced9dc78e2fcc3acb8c5ab87942b734bd4204
Harjit Kainth <harjitkainth10@gmail.com>
2021-06-22 11:37:13 -0700

    edit json file. update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 const winningImage = document.getElementById('win-image');
 const losingImage = document.getElementById('losing-image');
 const hangmanImage = document.getElementById('hangman-image');
+const wordOutput = document.getElementById('word');
+const hintOutput = document.getElementById('hint'); 
 
 hangmanImage.src = 'images/img-0.png';
 winningImage.src = 'images/celebration.jpg'
@@ -13,21 +15,37 @@ guessesColorChange();
 const wrongGuesses = document.getElementById("wrong-guesses");
 wrongGuesses.innerText = 0;
 
-const fetchFileURL = 'data/json/wordsapi_sample.json';
+const fetchFileURL = 'data/json/words.json';
 
 fetch(fetchFileURL)
     .then(function(response){
         console.log('response ok');
         if(response.ok){
-            let responseAsJson = response.json();
-            return responseAsJson;
+            const jsonData = response.json();
+            return jsonData;
+            
         }else{
             console.log('error with response');
         }
     })
-    .then(function( jsonData ){
+    .then(function(jsonData){
+        const data = jsonData[Math.floor(Math.random()*jsonData.length)];
         
+        const Word = {
+
+            word: data.word,
+            hint: data.hint
+            
+
+        }
+
+        wordOutput.innerHTML = Word.word;
+        hintOutput.innerText = Word.hint;
     })
     .catch(function(){
         console.log('error with response');
-    }); 
+    });
+    
+
+
+
